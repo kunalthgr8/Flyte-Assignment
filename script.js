854107ad6c2023f13adca43767e11c59b163be41
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-04-11 14:22:23 +0530

    Made minor changes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Hide error icons initially
 document.querySelectorAll(".error-icon").forEach(function (icon) {
   icon.style.display = "none";
 });
 
+// Listen for form submission
 document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
+  e.preventDefault(); // Prevent default form submission behavior
 
+  // Get input values
   var grossIncome = parseFloat(document.getElementById("gross-income").value);
   var extraIncome = parseFloat(document.getElementById("extra-income").value);
   var ageGroup = document.getElementById("age-group").value;
@@ -19,7 +22,6 @@ document.querySelector("form").addEventListener("submit", function (e) {
   if (isNaN(grossIncome) || isNaN(extraIncome) || isNaN(deductions)) {
     document.querySelectorAll(".error-icon").forEach(function (icon) {
       icon.style.display = "inline";
-      // Implement tooltip functionality here
     });
     return; // Prevent further execution
   }
@@ -30,18 +32,22 @@ document.querySelector("form").addEventListener("submit", function (e) {
     return; // Prevent further execution
   }
 
+  // Calculate overall income
   var overallIncome = grossIncome + extraIncome - deductions;
+
   if (overallIncome <= 800000) {
     document.getElementById("overall-income").innerText =
       "Total Income: " + overallIncome;
+    // Hide form and display success message
     document.getElementById("form-div").style.display = "none";
     document.getElementById("successDiv").style.display = "block";
-    return;
+    return; // Stop further execution
   }
 
-  // Calculate tax
+  // Calculate tax for income above non-taxable range
   if (overallIncome >= 800000) {
     var taxRate;
+    // Determine tax rate based on age group
     if (ageGroup === "<40") {
       taxRate = 0.3;
     } else if (ageGroup === ">=40&<60") {
@@ -49,16 +55,19 @@ document.querySelector("form").addEventListener("submit", function (e) {
     } else {
       taxRate = 0.1;
     }
+    // Calculate taxable income and tax amount
     var taxableIncome = grossIncome + extraIncome - deductions - 800000;
     var taxAmount = taxRate * taxableIncome;
 
     document.getElementById("overall-income").innerText =
       "Total Income: " + (800000 + (taxableIncome - taxAmount)) + " Lakhs";
+    // Hide form and display success message
     document.getElementById("form-div").style.display = "none";
     document.getElementById("successDiv").style.display = "block";
   }
 });
 
+// Function to close success message and reset form
 function closeDiv() {
   document.getElementById("successDiv").style.display = "none";
   document.getElementById("gross-income").value = "";
